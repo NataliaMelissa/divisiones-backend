d75d973472bab41ba49cb502b1dbe725e3a5ec50
NataliaMelissa <u201816996@upc.edu.pe>
2026-05-05 12:25:09 -0500

    Semilla BD

---
+++ src/docs/seeds.sql
@@ -0,0 +1,13 @@
+--Insert Data
+
+INSERT INTO division (nombre, embajador, padreId) VALUES 
+('Dirección General', 'Ana Martínez', NULL),      -- ID: 1
+('Operaciones', 'Carlos Ruiz', 1),                -- ID: 2
+('Estrategia y Finanzas', 'Elena Soler', 1),      -- ID: 3
+('Logística', 'Luis Páez', 2),                    -- ID: 4
+('Producción', 'Marta Tello', 2),                 -- ID: 5
+('Marketing', 'Jorge Luna', 3),                   -- ID: 6
+('Recursos Humanos', 'Sofía Villa', 3),           -- ID: 7
+('Almacén', 'Pedro Gómez', 4),                    -- ID: 8
+('Transporte', 'Lucía Sanz', 4),                  -- ID: 9
+('Publicidad Digital', 'Kevin Rojas', 6);         -- ID: 10
